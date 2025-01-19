d4fa480dd72b2dd58cc0549727b1e6ea2b33ee13
Fabio <fabioholsen@gmail.com>
2025-01-19 01:18:59 +0100

    Parte1Estilizada

--- 06Diccionarios/Reto.py
+++ 06Diccionarios/Reto.py
@@ -1,12 +1,15 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 muroL = len(muro)
 laberintoM = []
-print(muroL)
 for filaM in range(0,5):
     laberintoM.append([])
     for columnaM in range(0,5):
-        laberintoM[filaM].append(" ")
+        laberintoM[filaM].append("   ")
 for i in range(muroL):
-    laberintoM[muro[i][0]][muro[i][1]] = "X"
-
-print(laberintoM)
+    laberintoM[muro[i][0]][muro[i][1]] = "[X]"
+laberintoM[0][0] = "[E]"
+laberintoM[4][4] = "[S]"
+for x in laberintoM:
+        for y in x:
+            print(y,end=" ")
+        print()
